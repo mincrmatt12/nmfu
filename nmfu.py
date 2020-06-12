5cc1bcaa602e9c5869e35eeca08985a35097d161
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-06-12 15:31:38 -0400

    Bump version to 0.1.1

--- nmfu.py
+++ nmfu.py
@@ -18,7 +18,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 """
 
-__version__ = "0.1"
+__version__ = "0.1.1"
 
 import lark
 import abc
