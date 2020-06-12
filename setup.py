937b461879b06ef45571d7f3d1b08d40596c7b9e
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2020-06-12 02:33:03 -0400

    Add setup.py and release to PyPI

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+import sys
+import os
+
+# allow importing nmfu
+sys.path.append(os.path.dirname(__file__))
+
+from nmfu import __version__ as nmfu_version
+
+with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as f:
+    readme = f.read()
+
+setup(
+        name="nmfu",
+        version=nmfu_version,
+        py_modules=["nmfu"],
+        description="A parser generator that turns procedural programs into C state machines",
+        entry_points={
+            "console_scripts": ["nmfu=nmfu:main"]
+        },
+        install_requires=["lark-parser==0.8.*"],
+        author="Matthew Mirvish",
+        author_email="matthew@mm12.xyz",
+
+        license="GPLv3",
+        long_description=readme,
+        long_description_content_type="text/markdown",
+
+        keywords="c parser parser-generator cli tool",
+        url="https://github.com/mincrmatt12/nmfu",
+        project_urls={
+            "Bug Tracker": "https://github.com/mincrmatt12/nmfu/issues",
+            "Source Code": "https://github.com/mincrmatt12/nmfu"
+        },
+
+        extras_require={
+            "debug": ["graphviz==0.14"]
+        },
+        python_requires="~=3.6",
+        
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Environment :: Console",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Programming Language :: C",
+            "Topic :: Software Development :: Code Generators"
+        ]
+)
