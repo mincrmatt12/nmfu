f77ee728639eda44e5874a0e2d3ffcba8e3f049a
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2021-04-30 23:58:07 -0400

    Add various targeted full integration tests

--- tests/test_full_integration.py
+++ tests/test_full_integration.py
@@ -34,3 +34,42 @@ def test_full_integration(filename):
     cctx.generate_source()
     
     # yay
+
+example_files_ok   = glob.glob(os.path.join(os.path.dirname(__file__), "../example/test/*.ok.nmfu"))
+example_files_fail = glob.glob(os.path.join(os.path.dirname(__file__), "../example/test/*.fail.nmfu"))
+
+@pytest.mark.parametrize("filename", example_files_ok)
+def test_ok(filename):
+    # check if the file compiles correctly
+    with open(filename) as f:
+        source = f.read()
+        
+    nmfu.ProgramData.load_commandline_flags(("-O3", filename))
+    nmfu.ProgramData.load_source(source)
+
+    pt = nmfu.parser.parse(source)
+
+    pctx = nmfu.ParseCtx(pt)
+    pctx.parse()
+
+    dctx = nmfu.DfaCompileCtx(pctx)
+    dctx.compile()
+
+@pytest.mark.parametrize("filename", example_files_fail)
+def test_fail(filename):
+    # check if the file doesn't compile correctly
+    with open(filename) as f:
+        source = f.read()
+        
+    nmfu.ProgramData.load_commandline_flags(("-O3", filename))
+    nmfu.ProgramData.load_source(source)
+
+    # note that the files still should be valid syntax
+    pt = nmfu.parser.parse(source)
+    
+    with pytest.raises(nmfu.NMFUError):
+        pctx = nmfu.ParseCtx(pt)
+        pctx.parse()
+
+        dctx = nmfu.DfaCompileCtx(pctx)
+        dctx.compile()
