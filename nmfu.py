482a0880ae245239d7536947ea470adca5b5747c
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2021-05-03 12:55:09 -0400

    bump version to 0.3.0a2

--- nmfu.py
+++ nmfu.py
@@ -18,7 +18,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 """
 
-__version__ = "0.3.0a1"
+__version__ = "0.3.0a2"
 
 import abc
 import enum
